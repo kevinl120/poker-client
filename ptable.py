cbe5948b50a8a9c0935339635ebfc2da55633bc2
Kevin Li <kevinli120@yahoo.com>
2018-08-25 15:55:55 -0700

    Draw ptable

--- ptable.py
+++ ptable.py
@@ -1,7 +1,7 @@
-from tkinter import *
+import tkinter as tk
 
 class PokerTable:
-    def __init__(self, players):
+    def __init__(self, players=None):
         self.players = players
     
     def start(self):
@@ -10,6 +10,10 @@ class PokerTable:
         self.draw()
     
     def draw(self):
-        gui = Tk()
+        gui = tk.Tk()
+        gui.geometry("1002x743")
         gui.title("Poker Table")
+        background_image = tk.PhotoImage(file="./resources/table.png")
+        background_label = tk.Label(gui, image=background_image)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
         gui.mainloop()
